2353b224a45e6a0ad0b94aa8e6ee33236de41f4d
imdhj <imdhj1992@gmail.com>
2025-08-11 00:19:12 +0530

    fix portfoliocard

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from "framer-motion";
+import ProfilePic from '../assets/profile.jpg';
 const PortfolioCard = () => {
   return (
       <div className="flex-1 flex justify-center md:justify-end">
@@ -11,7 +12,7 @@ const PortfolioCard = () => {
               className="w-56 md:w-64 lg:w-72 rounded-2xl bg-gradient-to-tr from-indigo-50 to-white shadow-lg p-6 flex flex-col items-center"
             >
               <div className="w-28 h-28 rounded-full bg-slate-200 mb-4">
-                <img src="src\assets\profile.jpg" alt="Profile" className="w-full h-full rounded-full object-cover" />
+                <img src={ProfilePic} alt="Profile" className="w-full h-full rounded-full object-cover" />
               </div>
               <div className="text-center">
                 <div className="font-semibold">Himanshu Jain</div>
